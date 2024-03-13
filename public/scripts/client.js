91d65a324d52d3ac53307acc741f198509a69227
JackMac10 <macdonaldjackson0@gmail.com>
2024-03-13 14:07:00 -0700

    dynamic tweets: added temp data, renderTweets func & createTweets func to client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,64 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {;
+  // Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $('#tweets-container').append($tweet);
+  }
+};
+
+const createTweetElement = function(tweet) {
+  const $tweet = $(`
+    <article class="tweet">
+      <header>
+        <div class="buds">
+        <img src="${tweet.user.avatars}" alt="Profile Picture">
+        <h3>${tweet.user.name}</h3>
+        </div>
+        <span class="username">${tweet.user.handle}</span>
+      </header>
+      <p>${tweet.content.text}</p>
+      <hr>
+      <footer>
+        <span>${tweet.created_at}</span>
+        <div class="icons">
+          <i class="far fa-heart"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-flag"></i>
+        </div>
+      </footer>
+    </article>
+  `);
+  return $tweet;
+};
+
+renderTweets(data);
+});
